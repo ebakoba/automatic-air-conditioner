1a2d02aa463696ab97a5d04216e2cedd6347e1fc
Ebakoba <kaarelallemann@gmail.com>
2019-04-16 02:09:43 +0300

    fix database insertion

--- database.py
+++ database.py
@@ -9,6 +9,10 @@ CREATE TABLE IF NOT EXISTS switch (
 );
 """
 
+SWITCH_INSERT = """
+INSERT INTO switch (last_switch) VALUES (?)
+"""
+
 
 class DataBase:
     def __init__(self):
@@ -20,4 +24,6 @@ class DataBase:
 
     def database_setup(self):
         cursor = self.connection.cursor()
+        cursor.execute(SWITCH_INSERT, (1,))
         cursor.execute(SWITCH_SQL)
+        self.connection.commit()
