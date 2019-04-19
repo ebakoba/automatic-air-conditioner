fa86f22c061a653fe3c98266a7b882e47a68b8bd
Ebakoba <kaarelallemann@gmail.com>
2019-04-20 00:41:57 +0300

    fix typo

--- src/temperature.py
+++ src/temperature.py
@@ -4,7 +4,7 @@ class TemperatureService:
     def __init__(self, pin):
         self.pin = pin
 
-    def read_temperatur(self):
+    def read_temperature(self):
         temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, self.pin)[1]
 
         return temperature
